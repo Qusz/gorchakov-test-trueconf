9ff8899c30aa621cb625ae747ff127447adc9b53
Qusz <de.gorchakov@gmail.com>
2023-01-24 20:27:12 +0300

    sets up provide props

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,24 @@
 import Floor from '@/components/Floor/Floor.vue';
 import Lift from '@/components/Lift/Lift.vue';
 
-import { ref } from 'vue';
+import { ref, provide } from 'vue';
 
 //* Set the amount of floors
 const floors = Array.from({length: 5}, (_, index) => index + 1).reverse();
 
 const status = ref({
-  currentFloor: null,
+  currentFloor: 1,
   nextFloor: null,
   movingDirection: null,
-  liftStatus: null,
+  liftStatus: 'idle',
 });
 
 const queue = new Set();
+
+provide('nextFloor', status.value.nextFloor);
+provide('movingDirection', status.value.movingDirection);
+provide('liftStatus', status.value.liftStatus);
+
 
 </script>
 
