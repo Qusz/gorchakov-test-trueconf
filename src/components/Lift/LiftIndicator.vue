f9a8a1f3d9da8dc2ff1a553cb5a80f163a1abf16
Qusz <de.gorchakov@gmail.com>
2023-01-24 19:20:29 +0300

    adds lift-indicator

--- src/components/Lift/LiftIndicator.vue
+++ src/components/Lift/LiftIndicator.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>
-
+  <div class="lift-indicator">
+    <div class="lift-indicator__wrapper">
+      <div
+        v-show="direction === 'up'" 
+        class="lift-indicator__direction"
+      >
+        &#x2191;
+      </div>
+      <div
+        v-show="direction === 'down'" 
+        class="lift-indicator__direction"
+      >
+        &#x2193;
+      </div>
+      <div class="lift__indicator__target">
+        {{ nextFloor }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 
+import { defineProps } from 'vue';
+
+const props = {
+  nextFloor: Number,
+  direction: String
+}
+
 </script>
 
 <style lang="scss" scoped>
+
+@use '@/styles/base' as *;
+
+  .lift-indicator {
+    width: 50px;
+    height: 30px;
+    padding: 5px;
+
+    background-color: $clr-black;
+    color: $clr-white;
+    border-radius: 5px;
+
+      &__wrapper {
+        display: flex;
+        gap: 2px;
+        align-items: center;
+        justify-content: center;
+      }
+  }
 
 </style>
