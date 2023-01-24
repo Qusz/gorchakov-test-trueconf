b3a509bed0fa43e357e095e4bca0ba87ed295f74
Qusz <de.gorchakov@gmail.com>
2023-01-24 20:08:37 +0300

    sets up refs

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,19 @@
 import Floor from '@/components/Floor/Floor.vue';
 import Lift from '@/components/Lift/Lift.vue';
 
+import { ref } from 'vue';
+
+//* Set the amount of floors
 const floors = Array.from({length: 5}, (_, index) => index + 1).reverse();
 
+const status = ref({
+  currentFloor: null,
+  nextFloor: null,
+  movingDirection: null,
+  liftStatus: null,
+});
+
+const queue = new Set();
 
 </script>
 
