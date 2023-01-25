f3a94616a57f4ab6baf0cdb4cc8a980d59f2064f
Qusz <de.gorchakov@gmail.com>
2023-01-25 12:16:13 +0300

    adds moveLift function

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
       <Floor
         v-for="floor, index in floors" :key="index"
         :floorNumber="floor"
+        @call="moveLift(1, 3)"
       >
       </Floor>
-      <Lift class="floors__lift"></Lift>
+      <Lift :style="lift" class="floors__lift"></Lift>
     </div>
   </main>
 </template>
@@ -21,6 +22,10 @@ import { ref, provide } from 'vue';
 //* Set the amount of floors
 const floors = Array.from({length: 5}, (_, index) => index + 1).reverse();
 
+const lift = ref({
+  transform: 'translateY(0px)'
+});
+
 const status = ref({
   currentFloor: 1,
   nextFloor: null,
@@ -29,6 +34,18 @@ const status = ref({
 });
 
 const queue = new Set();
+
+const moveLift = (currentFloor, targetFloor) => {
+  // Set floor height in px
+  const floorHeight = 200;
+
+  const floorDifference = targetFloor - currentFloor;
+  const distance = floorDifference * floorHeight;
+
+  // 1 floor per second
+  lift.value.transition = `transform ${floorDifference}s linear`;
+  lift.value.transform = `translateY(-${distance}px)`;
+}
 
 provide('nextFloor', status.value.nextFloor);
 provide('movingDirection', status.value.movingDirection);
