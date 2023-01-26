82ad354fd3aa33f59e9fedcfa7b3c9b10c3a0350
Qusz <de.gorchakov@gmail.com>
2023-01-26 15:29:52 +0300

    fixes local storage bugs, implements generator

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,10 @@
       >
       </Floor>
       <Lift
+        ref="lift"
         @transitionend="handleTransition"
         @animationend="handleTransition"
-        :style="lift" 
+        :style="liftAnimations" 
         :class="[
           'floors__lift',
           { 'blink': status.liftStatus === 'recharge' }
@@ -33,7 +34,7 @@ import {
   watchEffect, 
   onMounted, 
   watch,
-  reactive 
+  reactive, 
 } from 'vue';
 
 class Queue {
@@ -64,7 +65,7 @@ const floorHeight = 200;
 
 const status = ref({
   currentFloor: 1,
-  currentPosition: 0,
+  targetPosition: 0,
   nextFloor: null,
   movingDirection: null,
   operationTime: 0,
@@ -72,48 +73,15 @@ const status = ref({
   floorCalled: {},
 });
 
-const queue = ref(new Queue());
-
-const lift = computed(() => {
+const liftAnimations = computed(() => {
   return {
-    bottom: status.value.currentPosition,
+    bottom: status.value.targetPosition,
     transition: `bottom ${status.value.operationTime}s linear`
   }
 });
 
-onMounted(() => {
-  // Set initial floorCalled values
-  if (Object.keys(status.value.floorCalled).length === 0) {
-    for (let i = 0; i < amountOfFloors; i++) {
-      status.value.floorCalled[i + 1] = false;
-    }
-  }
-
-  const storedStatus = localStorage.getItem('status');
-  if (storedStatus) {
-    Object.assign(status.value, JSON.parse(storedStatus));
-  }
-
-  const storedQueue = localStorage.getItem('queue');
-  if (storedQueue) {
-    Object.assign(status.value, JSON.parse(storedQueue));
-  }
-
-  if (status.value.liftStatus !== 'idle') {
-    complete();
-  }
-});
-
-watchEffect(() => {
-  if (!queue.value.queueIsEmpty() && status.value.liftStatus === 'idle') {
-    const iterator = queue.value.queue.values();
-    const next = iterator.next();
-
-    if (!next.done) {
-      handleCall(next.value);
-    }
-  }
-});
+const queue = ref(new Queue());
+const isMounted = ref(false);
 
 watch(status.value, (newVal, oldVal) => {
   localStorage.setItem('status', JSON.stringify(newVal));
@@ -133,7 +101,7 @@ const moveLift = (currentFloor, targetFloor) => {
   // 1 floor per second
   status.value.operationTime = floorDifference;
   // Lift positioning
-  status.value.currentPosition = `${(targetFloor - 1) * floorHeight}px`;
+  status.value.targetPosition = `${(targetFloor - 1) * floorHeight}px`;
 }
 
 const handleCall = (targetFloor) => {
@@ -142,15 +110,13 @@ const handleCall = (targetFloor) => {
     return;
   };
 
+  status.value.nextFloor = targetFloor;
   status.value.liftStatus = 'active';
   status.value.movingDirection = targetFloor > status.value.currentFloor 
     ? 'up' : 'down';
-  status.value.nextFloor = targetFloor;
 
   moveLift(status.value.currentFloor, targetFloor);
-  changeFloorStatus(status.value.currentFloor, false);
   status.value.currentFloor = status.value.nextFloor;
-  queue.value.deleteFromQueue(status.value.currentFloor);
 }
 
 const handleTransition = (e) => {
@@ -171,10 +137,62 @@ const changeFloorStatus = (floor, value) => {
 }
 
 const complete = () => {
-  changeFloorStatus(status.value.currentFloor, false);
+  queue.value.deleteFromQueue(status.value.nextFloor);
+
   status.value.nextFloor = null;
   status.value.movingDirection = null;
-  status.value.liftStatus = 'idle';  
+
+  status.value.liftStatus = 'idle';
+  status.value.operationTime = null;
+  changeFloorStatus(status.value.currentFloor, false);
+}
+
+const handleQueue = () => {
+  if (!queue.value.queueIsEmpty() && status.value.liftStatus === 'idle') {
+    const gen = queueIterator(queue.value.queue);
+    const next = gen.next();
+
+    if (!next.done) {
+      handleCall(next.value);
+    }
+  }
+}
+
+watchEffect(() => {
+  if (!isMounted.value) return;
+  handleQueue();
+});
+
+onMounted(() => {
+  // Set initial floorCalled values
+  if (Object.keys(status.value.floorCalled).length === 0) {
+    for (let i = 0; i < amountOfFloors; i++) {
+      status.value.floorCalled[i + 1] = false;
+    }
+  }
+
+  const storedStatus = localStorage.getItem('status');
+  if (storedStatus) {
+    Object.assign(status.value, JSON.parse(storedStatus));
+  }
+
+  const storedQueue = localStorage.getItem('queue');
+  if (storedQueue) {
+    Object.assign(queue.value, JSON.parse(storedQueue));
+  }
+
+  if (status.value.liftStatus === 'active') {
+    status.value.currentFloor = status.value.nextFloor;
+    status.value.liftStatus = 'recharge';
+  }
+
+  isMounted.value = true;
+});
+
+const queueIterator = function* (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    yield arr[i];
+  }
 }
 
 provide('status', status.value);
