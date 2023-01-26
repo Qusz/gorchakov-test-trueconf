4eedc14f317ef48ec1a6c829e830b3e591e1bd4f
Qusz <de.gorchakov@gmail.com>
2023-01-26 16:11:48 +0300

    clean up, comments

--- src/App.vue
+++ src/App.vue
@@ -41,11 +41,19 @@ import {
   onMounted, 
 } from 'vue';
 
+/* ================
+  Base variables
+================ */
+
 // Set the amount of floors
 const amountOfFloors = 5;
 const floors = Array.from({length: amountOfFloors}, (_, index) => index + 1).reverse();
 // Floor height in px
 const floorHeight = 200;
+
+/* ================
+  Reactive data
+================ */
 
 const status = ref({
   currentFloor: 1,
@@ -57,6 +65,7 @@ const status = ref({
   floorCalled: {},
 });
 
+// To apply animation styles to Lift
 const liftAnimations = computed(() => {
   return {
     bottom: status.value.targetPosition,
@@ -64,75 +73,14 @@ const liftAnimations = computed(() => {
   }
 });
 
+// Queue handler
 const queue = ref(new Queue());
+// Mounted flag
 const isMounted = ref(false);
 
-/* =============
-  Functions
-============= */
-
-const moveLift = (currentFloor, targetFloor) => {
-  // Floor difference always must be positive
-  const floorDifference = Math.abs(targetFloor - currentFloor);
-  // 1 floor per second
-  status.value.operationTime = floorDifference;
-  // Lift positioning
-  status.value.targetPosition = `${(targetFloor - 1) * floorHeight}px`;
-}
-
-const handleCall = (targetFloor) => {
-  if (targetFloor === status.value.currentFloor) {
-    queue.value.deleteFromQueue(targetFloor);
-    return;
-  };
-
-  status.value.nextFloor = targetFloor;
-  status.value.liftStatus = 'active';
-  status.value.movingDirection = targetFloor > status.value.currentFloor 
-    ? 'up' : 'down';
-
-  moveLift(status.value.currentFloor, targetFloor);
-  status.value.currentFloor = status.value.nextFloor;
-}
-
-const handleTransition = (e) => {
-  switch(true) {
-    case e.propertyName === 'bottom':
-      status.value.liftStatus = 'recharge';
-      break;
-    case e.animationName === 'blink-eb80ee3d':
-      jobComplete();
-      break;
-  }
-}
-
-const changeFloorStatus = (floor, value) => {
-  // To prevent status change if the lift is already on the floor
-  if (value === true && floor === status.value.currentFloor) return;
-  status.value.floorCalled[floor] = value;
-}
-
-const jobComplete = () => {
-  queue.value.deleteFromQueue(status.value.nextFloor);
-
-  status.value.nextFloor = null;
-  status.value.movingDirection = null;
-  status.value.operationTime = null;
-  
-  status.value.liftStatus = 'idle';
-  changeFloorStatus(status.value.currentFloor, false);
-}
-
-const handleQueue = () => {
-  if (!queue.value.queueIsEmpty() && status.value.liftStatus === 'idle') {
-    const gen = queueIterator(queue.value.queue);
-    const next = gen.next();
-
-    if (!next.done) {
-      handleCall(next.value);
-    }
-  }
-}
+/* ================
+  Hooks
+================ */
 
 watchEffect(() => {
   // To make sure onMounted logic runs first
@@ -158,10 +106,67 @@ onMounted(() => {
   isMounted.value = true;
 });
 
+/* ================
+  Functions
+================ */
+
+const moveLift = (currentFloor, targetFloor) => {
+  // Floor difference always must be positive
+  const floorDifference = Math.abs(targetFloor - currentFloor);
+  // 1 floor per second
+  status.value.operationTime = floorDifference;
+  // Lift positioning
+  status.value.targetPosition = `${(targetFloor - 1) * floorHeight}px`;
+}
+
+const changeFloorStatus = (floor, value) => {
+  // To prevent status change if Lift is already on the floor
+  if (value === true && floor === status.value.currentFloor) return;
+  status.value.floorCalled[floor] = value;
+}
+
+const jobComplete = () => {
+  queue.value.deleteFromQueue(status.value.nextFloor);
+
+  status.value.nextFloor = null;
+  status.value.movingDirection = null;
+  status.value.operationTime = null;
+
+  status.value.liftStatus = 'idle';
+  changeFloorStatus(status.value.currentFloor, false);
+}
+
 const queueIterator = function* (arr) {
   for (let i = 0; i < arr.length; i++) {
     yield arr[i];
   }
+}
+
+const handleQueue = () => {
+  if (!queue.value.queueIsEmpty() && status.value.liftStatus === 'idle') {
+    const gen = queueIterator(queue.value.queue);
+    const next = gen.next();
+
+    if (!next.done) {
+      handleCall(next.value);
+    }
+  }
+}
+
+const handleCall = (targetFloor) => {
+  // Check if Lift is already on the called floor
+  if (targetFloor === status.value.currentFloor) {
+    queue.value.deleteFromQueue(targetFloor);
+    return;
+  };
+
+  status.value.nextFloor = targetFloor;
+  status.value.liftStatus = 'active';
+  status.value.movingDirection = targetFloor > status.value.currentFloor 
+    ? 'up' : 'down';
+
+  moveLift(status.value.currentFloor, targetFloor);
+  status.value.currentFloor = status.value.nextFloor;
 }
 
 const handleLocalStorage = () => {
@@ -183,8 +188,18 @@ const handlePageRefresh = () => {
   }
 }
 
-provide('status', status.value);
+const handleTransition = (e) => {
+  switch(true) {
+    case e.propertyName === 'bottom':
+      status.value.liftStatus = 'recharge';
+      break;
+    case e.animationName === 'blink-eb80ee3d':
+      jobComplete();
+      break;
+  }
+}
 
+provide('status', status.value);
 
 </script>
 
